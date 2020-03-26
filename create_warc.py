9ee5d31b3743e907c7e0b7c90dbd97bf847e7efe
Shantanu <shantanu@senic.com>
2020-03-26 11:05:44 +0530

    Avoid fetching URLs that have trailing / or www subdomain

--- create_warc.py
+++ create_warc.py
@@ -8,6 +8,36 @@ from warcio.capture_http import capture_http
 import requests
 from requests_html import HTML
 
+def check_url_similarity(url_1, url_2):
+    def check_path(path_1, path_2):
+        # handles cases: cases where path are similar of have a trailing /
+        if path_1 == path_2:
+            return True
+        if path_1 == path_2+'/' or \
+               path_1+'/' == path_2:
+            return True
+        else:
+            return False
+
+    if len(url_2) == len(url_1):
+        if url_1 == url_2:
+            return True
+    else:
+        netloc_1 = urlparse(url_1).netloc
+        netloc_2 = urlparse(url_2).netloc
+        path_1 = urlparse(url_1).path
+        path_2 = urlparse(url_2).path
+        if netloc_1 == netloc_2:
+            if check_path(path_1, path_2):
+                return True
+        if netloc_1 == 'www.'+netloc_2 or \
+           'www.'+netloc_1 == netloc_2:
+            if check_path(path_1, path_2):
+                return True
+        # TODO:
+        # 1. add case for http and https distinction
+    return False
+
 if __name__ == "__main__":
     warc_file = '{}.warc.gz'.format(sys.argv[2])
     with capture_http(warc_file):
@@ -19,6 +49,11 @@ if __name__ == "__main__":
         style = set(styles)
         images = set([elem.attrs['src'] for elem in html.find('img') if 'src' in elem.attrs])
         for static_resource in set.union(js_scripts, styles, images):
+            if not urlparse(static_resource).hostname:
+                continue
+            if check_url_similarity(static_resource, html.url):
+                print('skipping: {}'.format(static_resource))
+                continue
             if not urlparse(static_resource).scheme:
                 static_resource = 'https:'+static_resource
             print('downloading resource: {}'.format(static_resource))
