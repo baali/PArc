9cb37cbbf46df8c04639b470eb197c9f382b3790
Shantanu <shantanu@senic.com>
2020-03-28 15:42:06 +0530

    Create methods to download static content.
    closes #1

--- create_warc.py
+++ create_warc.py
@@ -36,17 +36,41 @@ def check_url_similarity(url_1, url_2):
                 return True
     return False
 
+def find_js_urls(html):
+    '''Find urls to all JS files used in the HTML'''
+    js_urls = [elem.attrs['src'] for elem in html.find('script') if 'src' in elem.attrs]
+    print("Found {} urls for Javascripts".format(len(js_urls)))
+    return js_urls
+
+def find_img_urls(html):
+    '''Find urls to all images being used in the HTML'''
+    img_urls = [elem.attrs['src'] for elem in html.find('img') if 'src' in elem.attrs]
+    print("Found {} urls for images".format(len(img_urls)))
+    return img_urls
+
+def find_css_urls(html):
+    '''Find urls to all stylesheets used in the HTML'''
+    css_urls = [elem.attrs['src'] for elem in html.find('style') if 'src' in elem.attrs]
+    css_urls += [elem.attrs['href'] for elem in html.find('link') if 'href' in elem.attrs]
+    print("Found {} urls for CSS files".format(len(css_urls)))
+    return css_urls
+
+def get_all_static_urls(html):
+    static_urls = set.union(
+        set(find_js_urls(html)),
+        set(find_img_urls(html)),
+        set(find_css_urls(html))
+        )
+    return static_urls
+
+
 if __name__ == "__main__":
     warc_file = '{}.warc.gz'.format(sys.argv[2])
     with capture_http(warc_file):
         response = requests.get(sys.argv[1])
         html = HTML(html=response.text, url=response.url)
-        js_scripts = set([elem.attrs['src'] for elem in html.find('script') if 'src' in elem.attrs])
-        styles = [elem.attrs['src'] for elem in html.find('style') if 'src' in elem.attrs]
-        styles += [elem.attrs['href'] for elem in html.find('link') if 'href' in elem.attrs]
-        style = set(styles)
-        images = set([elem.attrs['src'] for elem in html.find('img') if 'src' in elem.attrs])
-        for static_resource in set.union(js_scripts, styles, images):
+        static_resource_urls = get_all_static_urls(html)
+        for static_resource in static_resource_urls:
             if not urlparse(static_resource).hostname:
                 continue
             if check_url_similarity(static_resource, html.url):
